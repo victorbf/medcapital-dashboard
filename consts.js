d6ca580ab74dd5b3cb3606aba444d6788ad73118
victorbf <victorbeiraof@gmail.com>
2019-09-02 01:02:57 -0300

    First commit

---
+++ consts.js
@@ -0,0 +1,105 @@
+const financias_total = [
+  {
+    id: 1,
+    nome: "Total rendimento bruto",
+    total: 244300.0
+  },
+  {
+    id: 2,
+    nome: "Total impostos pagos",
+    total: 29214.71
+  },
+  {
+    id: 1,
+    nome: "Total alíquota media",
+    total: 11.96
+  }
+];
+
+const financias = [
+  {
+    vinculo: {
+      id: 1,
+      nome: "PF"
+    },
+    fonte: {
+      id: 1,
+      nome: "Hospital A"
+    },
+    tipo_fonte: {
+      id: 1,
+      nome: "Hospital/Convênios"
+    },
+    rendimento_bruto: 59500.0,
+    impostos_pagos: 9059.87,
+    aliquota_media: 15.23
+  },
+  {
+    vinculo: {
+      id: 1,
+      nome: "PF"
+    },
+    fonte: {
+      id: 2,
+      nome: "Plano de Saúde A"
+    },
+    tipo_fonte: {
+      id: 1,
+      nome: "Hospital/Convênios"
+    },
+    rendimento_bruto: 24500.0,
+    impostos_pagos: 694.7,
+    aliquota_media: 2.84
+  },
+  {
+    vinculo: {
+      id: 1,
+      nome: "PF"
+    },
+    fonte: {
+      id: 3,
+      nome: "Plano de Saúde B"
+    },
+    tipo_fonte: {
+      id: 1,
+      nome: "Hospital/Convênios"
+    },
+    rendimento_bruto: 7000.0,
+    impostos_pagos: null,
+    aliquota_media: 0
+  },
+  {
+    vinculo: {
+      id: 2,
+      nome: "PJ"
+    },
+    fonte: {
+      id: 4,
+      nome: "Consultório"
+    },
+    tipo_fonte: {
+      id: 2,
+      nome: "Particular"
+    },
+    rendimento_bruto: 17500.0,
+    impostos_pagos: null,
+    aliquota_media: 0
+  },
+  {
+    vinculo: {
+      id: 2,
+      nome: "PJ"
+    },
+    fonte: {
+      id: 5,
+      nome: "Hospital C"
+    },
+    tipo_fonte: {
+      id: 1,
+      nome: "Hospital/Convênios"
+    },
+    rendimento_bruto: 49000.0,
+    impostos_pagos: 7021.7,
+    aliquota_media: 14.33
+  }
+];
